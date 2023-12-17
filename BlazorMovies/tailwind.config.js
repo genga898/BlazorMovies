b8a5bd0d1d37820d74344193a43dd67bbf70b0bc
Emmanuel Genga <genga898@gmail.com>
2023-12-17 11:49:19 +0300

    Updated the tailwind config

--- BlazorMovies/tailwind.config.js
+++ BlazorMovies/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './src/**/*.razor',
+        './**/**/*.razor', "**/*.html",
     ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
 
